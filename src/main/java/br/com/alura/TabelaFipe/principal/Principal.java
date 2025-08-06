1b1f1e8110fe13b48946375e0c7b5cc92724948b
LuizCarlosGomesJunior <luizc.gomes.jr@hotmail.com>
2025-08-06 17:17:24 -0300

    Primeiro commit do projeto para pesquisa da tabela Fipe

---
+++ src/main/java/br/com/alura/TabelaFipe/principal/Principal.java
@@ -0,0 +1,46 @@
+package br.com.alura.TabelaFipe.principal;
+
+import br.com.alura.TabelaFipe.model.Dados;
+import br.com.alura.TabelaFipe.service.ConsumoApi;
+import br.com.alura.TabelaFipe.service.ConverteDados;
+
+import java.util.Scanner;
+
+public class Principal {
+    private Scanner leitura = new Scanner(System.in);
+    private ConsumoApi consumoApi = new ConsumoApi();
+    private ConverteDados converteDados = new ConverteDados();
+
+    private final String URL_BASE = "https://parallelum.com.br/fipe/api/v1/";
+
+    public void exibeMenu(){
+        var menu = """
+                *** OPÇÕES ***
+                Carro
+                Moto
+                Caminhão
+                
+                Digite uma das opções para consulta:
+                
+                """;
+
+        System.out.println(menu);
+        var opcao = leitura.nextLine();
+        String endereco;
+
+        if (opcao.toLowerCase().contains("carro")) {
+            endereco = URL_BASE + "carros/marcas";
+        } else if (opcao.toLowerCase().contains("mot")){
+            endereco = URL_BASE + "motos/marcas";
+        } else {
+            endereco = URL_BASE + "caminhoes/marcas";
+        }
+
+        var json = consumoApi.obterDados(endereco);
+        System.out.println(json);
+
+
+
+    }
+
+}
